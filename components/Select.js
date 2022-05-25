8fc7c4ba4a5607eefa404c39fd9048a607594ed5
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-25 18:58:12 +0900

    components folder and Select component created

---
+++ components/Select.js
@@ -0,0 +1,20 @@
+import { FormControl, InputLabel, MenuItem } from "@mui/material"
+
+export default function Select() {
+  return (
+    <FormControl fullWidth>
+      <InputLabel id="demo-simple-select-label">Age</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={age}
+        label="Age"
+        onChange={handleChange}
+      >
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
