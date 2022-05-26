555ab9cb4d947c31e6229e21fdab4057c37cb9da
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-26 16:41:30 +0900

    datepicker now always in mobile mode

--- components/basic-date-picker.js
+++ components/basic-date-picker.js
@@ -2,14 +2,14 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 
 export default function BasicDatePicker() {
   const [value, setValue] = React.useState(null);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DatePicker
+      <MobileDatePicker
         label="Arrival date"
         value={value}
         onChange={(newValue) => {
