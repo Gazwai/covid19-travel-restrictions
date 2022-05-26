f521450328888cb0bf3e612b5ea03cc52e5aaac7
Camille Fujisaki <cae224@cornell.edu>
2022-05-27 07:09:20 +0900

    axios fetch request working for test api omdb

--- pages/components/Fetch.js
+++ pages/components/Fetch.js
@@ -1,32 +1,51 @@
 // import axios from 'axios';
 // import React from 'react';
+const axios = require('axios');
 
-export default class CountryData extends React.Component {
-  state = {
-    country: {}
-  }
+axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=41bb394e')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+  // .then(function () {
+  //   // always executed
+  // }
+  // );
 
-  componentDidMount() {
-    axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=41bb394e`)
-      .then(res => {
-        const country = res.data;
-        this.setState({ country });
-      })
-  }
 
-  render() {
-    return (
-      <ul>
-        {
-          this.state.country
-            .map(c =>
-              <li key={c}>{c}</li>
-            )
-        }
-      </ul>
-    )
-  }
-}
+
+
+
+// export default class CountryData extends React.Component {
+//   state = {
+//     country: {}
+//   }
+
+//   componentDidMount() {
+//     axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=41bb394e`)
+//       .then(res => {
+//         const country = res.data;
+//         this.setState({ country });
+//       })
+//   }
+
+//   render() {
+//     return (
+//       <ul>
+//         {
+//           this.state.country
+//             .map(c =>
+//               <li key={c}>{c}</li>
+//             )
+//         }
+//       </ul>
+//     )
+//   }
+// }
 
 // export default function Fetch() {
 //   axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=41bb394e')
